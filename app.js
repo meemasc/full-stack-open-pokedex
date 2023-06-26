1e6faf2907a2991abe7a569cc434857b45f3abc0
Mehmet Emin <meminascioglu@gmail.com>
2023-06-26 19:03:32 +0300

    maked changes for deployment

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.static('dist'))
 
+
 app.listen(PORT, () => {
-  console.log('server started on port 5000') // eslint-disable-line
+  console.log(`server started on port ${PORT}`) // eslint-disable-line
 })

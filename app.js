74fa38761715e9834f7e051dcf9c7f8dde388583
Mehmet Emin <meminascioglu@gmail.com>
2023-07-10 10:55:16 +0300

    added health tests that fail for testing

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('12')
+})
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line

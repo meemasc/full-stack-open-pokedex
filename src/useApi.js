1babcfd830bc1caeb1c9a0f06f47a158024ca56b
Mehmet Emin <meminascioglu@gmail.com>
2023-07-11 18:39:04 +0300

    added discord-webhook-norify and testing build faili ure

--- src/useApi.js
+++ src/useApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const useApi = (url, mapResults = (result) => result) => {
   const [data, setData] = useState()
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState()
   useEffect(() => {
     setIsLoading(true)
